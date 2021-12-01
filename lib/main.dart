c5bf65e6506e0e91f455a795822519701f765b59
DavidLee <tisdalearmstrong@gmail.com>
2021-12-01 16:56:10 +0800

    first

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//this is the main function just like in Java
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
