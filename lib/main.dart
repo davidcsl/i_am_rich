13aece7833a08fa699d5bdf6ef347b64e3cd1bfa
davidcsl <singaporecsl123@gmail.com>
2021-12-13 20:44:08 +0800

    Added_Images

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text("I am Rich"),
+        ),
+        backgroundColor: Colors.green,
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                "https://akm-img-a-in.tosshub.com/indiatoday/christmas-647-x-404_122415034930_122417033943_0.jpg?meMni6Y2Zy05lNbGY0cwq.3XbU83Fdnj&size=770:433"),
+          ),
+        ),
       ),
     ),
   );
